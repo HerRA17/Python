d5a2abe64fb4d63fe817249d61fdfd635359832c
Hermann Rasch <hermann.rasch93@gmail.com>
2023-10-26 20:43:58 +0200

    Delete add.py

--- exercise 1_1/add.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Provide initial value to be added: "))
-b = int(input("Provide second value to be added: "))
-third_value = int(input("Add a value that will be multiplied to the result: "))
-c = a + b
-addition_multiplication = c * third_value
-print(c)
-print( addition_multiplication)
